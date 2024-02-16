5a9f93e36e4c40ea0f62f79b9b224614e6eed29b
SoloDeveloperAlpha <112835682+SoloDeveloperAlpha@users.noreply.github.com>
2024-02-16 10:33:51 -0500

    Actualizar Archivos

--- src/Componentes/nav.jsx
+++ src/Componentes/nav.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import '../Estilos-Componentes/nav.scss';
 
 function Navegador(){
+
+  const [menuActivo, setMenuActivo] = useState(false);
+  const alClick = () => {
+    setMenuActivo(!menuActivo);
+  };
   return(
     <div className="nav">
+      <div className="nav-container">
+        <ul>
+          <li><a href="#sobremi">SOBRE MI</a></li>
+          <li><a href="#hab">HABILIDADES</a></li>
+          <li><a href="#proy">PROYECTOS</a></li>
+          <li><a href="https://drive.google.com/drive/folders/16m4DI0wse4F_YN-NQwPNv-fMr89JM3WS?usp=drive_link" target="_blank">CERTIFICADOS</a></li>
+        </ul>
+        <div className="menu-boton" id="menu-boton" onClick={alClick}>
+        <i className="fas fa-bars"></i>
+      </div>
+      </div>
+      <div className={`nav-despl ${menuActivo ? 'activo' : ''}`}>
       <ul>
-        <li><a href="#sobremi">SOBRE MI</a></li>
-        <li><a href="#hab">HABILIDADES</a></li>
-        <li><a href="#proy">PROYECTOS</a></li>
-        <li><a href="https://drive.google.com/drive/folders/16m4DI0wse4F_YN-NQwPNv-fMr89JM3WS?usp=drive_link" target="_blank">CERTIFICADOS</a></li>
-      </ul>
-      <div className="menu-boton">
-      <i className="fas fa-bars"></i>
+          <li><a href="#sobremi">SOBRE MI</a></li>
+          <li><a href="#hab">HABILIDADES</a></li>
+          <li><a href="#proy">PROYECTOS</a></li>
+          <li><a href="https://drive.google.com/drive/folders/16m4DI0wse4F_YN-NQwPNv-fMr89JM3WS?usp=drive_link" target="_blank">CERTIFICADOS</a></li>
+        </ul>
       </div>
     </div>
   );
 }
+
+
 
 export default Navegador;
